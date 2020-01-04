a93e4b25c97055f8169366d25bc9f17445d65dc2
tom <123@qq.com>
2020-01-04 22:26:11 +0800

    实现登录功能

--- gmall-admin-web/src/main/java/com/atguigu/gmall/admin/GmallAdminWebApplication.java
+++ gmall-admin-web/src/main/java/com/atguigu/gmall/admin/GmallAdminWebApplication.java
@@ -2,8 +2,11 @@ package com.atguigu.gmall.admin;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 
-@SpringBootApplication
+import javax.sql.DataSource;
+
+@SpringBootApplication(exclude = {DataSourceAutoConfiguration.class})
 public class GmallAdminWebApplication {
 
 	public static void main(String[] args) {
