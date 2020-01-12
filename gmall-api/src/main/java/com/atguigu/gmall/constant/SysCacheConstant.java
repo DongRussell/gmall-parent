28e3bfab7681aec00edb7ab60daca5a8a8de248b
tom <123@qq.com>
2020-01-12 22:43:10 +0800

    单点登录sso完成

---
+++ gmall-api/src/main/java/com/atguigu/gmall/constant/SysCacheConstant.java
@@ -0,0 +1,12 @@
+package com.atguigu.gmall.constant;
+
+/**
+ * @author Brodie
+ * @date 2020/1/12 - 21:01
+ */
+public class SysCacheConstant {
+    public static final String LOGIN_MEMBER = "login:member:";
+
+    public static final Long LOGIN_MEMBER_TIMEOUT = 30L;
+
+}
